4b3a742b09792cdcb6557cf7e3b340544e9dfd45
Robert <rc3331@gmail.com>
2019-08-22 14:36:44 -0500

    classes complete for lambda project

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,6 +1,64 @@
 // CODE here for your Lambda Classes
 
+class Person{
+    constructor(att){
+        this.name = att.name
+        this.age = att.age
+        this.location = att.location
+    }
+    speak(){
+        return `Hello, my name is ${this.name} and I'm from ${this.location}.`
+    }
+}
 
+class Instructor extends Person{
+    constructor(att){
+        super(att)
+        this.specialty = att.specialty
+        this.favLang = att.favLang
+        this.catchPhrase = att.catchPhrase
+    }
+    demo(subject){
+        return `Today we are learning about ${subject}.`
+    }
+    grade(student, subject){
+        return `${student} recieves a perfect score on ${subject}`
+    }
+}
+
+class Student extends Person{
+    constructor(att){
+        super(att)
+        this.prevBG = att.prevBG
+        this.className = att.className
+        this.favSubjects = att.favSubjects
+    }
+    listSubjects(){
+        for (i=0; i<this.favSubjects.length; i++){
+            console.log(this.favSubjects[i])
+        }
+    }
+    PRAssignment(subject){
+        return `${this.name} has submitted a PR for ${subject}.`
+    }
+    sprint(subject){
+        return `${this.name} has begun a sprint on ${subject}.`
+    }
+}
+
+class TeamLead extends Instructor{
+    constructor(att){
+        super(att)
+        this.GradClass = att.GradClass
+        this.favInstructor = att.favInstructor
+    }
+    standup(slackChannel){
+        return `${this.name} announces to ${slackChannel}, @channel, standy times!`
+    }
+    debug(student, subject){
+        return `${this.name} debugs ${student.name}'s code on ${subject}`
+    }
+}
 
 
 
