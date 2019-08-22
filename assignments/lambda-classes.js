708406e13d5090a88675fa24536460c96406536a
Robert <rc3331@gmail.com>
2019-08-22 15:00:33 -0500

    committing mvp and stretch for lambda projects

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -32,17 +32,19 @@ class Student extends Person{
         this.prevBG = att.prevBG
         this.className = att.className
         this.favSubjects = att.favSubjects
+        this.grade = att.grade
     }
     listSubjects(){
-        for (i=0; i<this.favSubjects.length; i++){
-            console.log(this.favSubjects[i])
-        }
+        return this.favSubjects.forEach(i => console.log(i))
     }
     PRAssignment(subject){
         return `${this.name} has submitted a PR for ${subject}.`
     }
     sprint(subject){
         return `${this.name} has begun a sprint on ${subject}.`
+    }
+    graduate(){
+        return this.grade > 70 ? "You've Graduated! Go get a job!" : "Sorry, we're going to have to hold you back."
     }
 }
 
@@ -58,13 +60,59 @@ class TeamLead extends Instructor{
     debug(student, subject){
         return `${this.name} debugs ${student.name}'s code on ${subject}`
     }
+    gradeAssignment(student){
+        let score = parseInt((Math.random() * 100), 10)
+        return `${this.name} just graded ${student.name}'s assignment. His score is ${score}!`
+    }
 }
 
 
+const britt = new Instructor({
+    name: "Brit Hemming",
+    age: "None of my business",
+    location: "Canada",
+    specialty: "CSS",
+    favLang: "HTML5",
+    catchPhrase: "I'm going to use my dog Ada for this next example."
+})
+
+console.log(britt.speak())
+console.log(britt.demo("JavaScript"))
+console.log(britt.grade("Robert", "JavaScript"))
 
 
+const robert = new Student({
+    name: "Robert Carter",
+    age: 28,
+    location: "Tulsa",
+    prevBG: "Finance",
+    className: "Web23",
+    favSubjects: ["Python", "Computer Science", "JavaScript"],
+    grade: 99
+})
+
+console.log(robert.listSubjects())
+console.log(robert.PRAssignment("JavaScript"))
+console.log(robert.sprint("Python"))
+
+const cford = new TeamLead({
+    name: "Christian Ford",
+    age: 21,
+    location: "Tay-Hass",
+    specialty: "React",
+    favLang: "English, duh",
+    catchPhrase: "Alright y'all I'm gonna be right back one second.",
+    GradClass: "WebPT Something",
+    favInstructor: "Dr. Pepper",
+})
+
+console.log(cford.standup("Slackfdfdfiab"))
+console.log(cford.debug(robert, "JavaScript"))
 
 
+// STRETCH
 
-
+console.log(robert.grade)
+console.log(robert.graduate())
+console.log(cford.gradeAssignment(robert))
 
